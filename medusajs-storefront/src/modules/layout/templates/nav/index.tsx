504bcd6a526630e548b7888df2667ad43e825e33
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2024-03-19 16:50:46 +0100

    :bug: Work

--- medusajs-storefront/src/modules/layout/templates/nav/index.tsx
+++ medusajs-storefront/src/modules/layout/templates/nav/index.tsx
@@ -6,67 +6,125 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
 import Logo from "./logo"
+import { XMarkIcon } from "@heroicons/react/20/solid"
 export default async function Nav() {
   const regions = await listRegions().then((regions) => regions)
 
   return (
-    <div className="sticky top-0 inset-x-0 z-50 group ">
-      <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
-        <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
-          <div className="flex-1 basis-0 h-full flex items-center">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-            >
-              <Logo />
-            </LocalizedClientLink>
-          </div>
-
-          <div className="flex items-center h-full">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-            >
-              Aqua Serwis System
-            </LocalizedClientLink>
-          </div>
-
-          <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <div className="hidden small:flex items-center gap-x-6 h-full">
-              {process.env.FEATURE_SEARCH_ENABLED && (
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base"
-                  href="/search"
-                  scroll={false}
-                >
-                  Szukaj
-                </LocalizedClientLink>
-              )}
+    <>
+      <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+        <div
+          className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+            }}
+          />
+        </div>
+        <div
+          className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+            }}
+          />
+        </div>
+        <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+          <p className="text-sm leading-6 text-gray-900">
+            <strong className="font-semibold">
+              Tylko teraz 6 miesięcy za darmo przy umowie na 2 lata
+            </strong>
+            {/* <svg
+          viewBox="0 0 2 2"
+          className="mx-2 inline h-0.5 w-0.5 fill-current"
+          aria-hidden="true"
+        >
+          <circle cx={1} cy={1} r={1} />
+        </svg> */}
+          </p>
+          <a
+            href="#"
+            className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+          >
+            Sprawdź teraz <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <button
+            type="button"
+            className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+          >
+            <span className="sr-only">Dismiss</span>
+            <XMarkIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
+          </button>
+        </div>
+      </div>
+      <div className="sticky top-0 inset-x-0 z-50 group ">
+        <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
+          <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
+            <div className="flex-1 basis-0 h-full flex items-center">
               <LocalizedClientLink
-                className="hover:text-ui-fg-base"
-                href="/account"
+                href="/"
+                className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
               >
-                Konto
+                <Logo />
               </LocalizedClientLink>
             </div>
-            <Suspense
-              fallback={
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base flex gap-2"
-                  href="/cart"
-                >
-                  Koszyk (0)
-                </LocalizedClientLink>
-              }
-            >
-              <CartButton />
-            </Suspense>
-            <div className="h-full">
-              <SideMenu regions={regions} />
+
+            <div className="flex items-center h-full">
+              <LocalizedClientLink
+                href="/"
+                className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
+              >
+                Aqua Serwis System
+              </LocalizedClientLink>
             </div>
-          </div>
-        </nav>
-      </header>
-    </div>
+
+            <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
+              <div className="hidden small:flex items-center gap-x-6 h-full">
+                {process.env.FEATURE_SEARCH_ENABLED && (
+                  <LocalizedClientLink
+                    className="hover:text-ui-fg-base"
+                    href="/search"
+                    scroll={false}
+                  >
+                    Szukaj
+                  </LocalizedClientLink>
+                )}
+                <LocalizedClientLink
+                  className="hover:text-ui-fg-base"
+                  href="/account"
+                >
+                  Konto
+                </LocalizedClientLink>
+              </div>
+              <Suspense
+                fallback={
+                  <LocalizedClientLink
+                    className="hover:text-ui-fg-base flex gap-2"
+                    href="/cart"
+                  >
+                    Koszyk (0)
+                  </LocalizedClientLink>
+                }
+              >
+                <CartButton />
+              </Suspense>
+              <div className="h-full">
+                <SideMenu regions={regions} />
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
+    </>
   )
 }
