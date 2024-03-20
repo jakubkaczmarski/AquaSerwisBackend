6496934a9542456920eb18413156f563b32fec63
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2024-03-20 09:16:32 +0100

    :art: Chuj

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -1,12 +1,12 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
-import Video from "next-video"
+// import Video from "next-video"
 import videoLoop from "../../../../../videos/video.mp4"
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="h-[50vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-        <Video
+        {/* <Video
           src={videoLoop}
           autoPlay
           muted
@@ -15,7 +15,7 @@ const Hero = () => {
           controls={false}
           // style={{ mediaObjectFit: "cover" }}
           className
-        />
+        /> */}
       </div>
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
